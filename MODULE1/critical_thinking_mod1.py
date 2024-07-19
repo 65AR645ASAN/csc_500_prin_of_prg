da480e6031ece23c438108c0c31e6b57c4e22bb4
65AR645ASAN <adi.sandhu@outlook.com>
2024-07-19 14:37:56 -0600

    Updates

---
+++ MODULE1/critical_thinking_mod1.py
@@ -0,0 +1,19 @@
+# Addition and Subtraction Program
+
+# Step 2: Prompt the user to enter the first number
+num1 = float(input("Enter the first number: "))
+
+# Step 3: Prompt the user to enter the second number
+num2 = float(input("Enter the second number: "))
+
+# Step 5: Calculate the sum of num1 and num2
+sum_result = num1 + num2
+
+# Step 6: Calculate the difference of num1 and num2
+sub_result = num1 - num2
+
+# Step 7: Print sum_result
+print(f"The sum of {num1} and {num2} is: {sum_result}")
+
+# Step 8: Print sub_result
+print(f"The difference of {num1} and {num2} is: {sub_result}")
