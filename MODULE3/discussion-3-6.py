3d124f692bc174bd1f15a2ac6f0bd8cf25abf74b
65AR645ASAN <adi.sandhu@outlook.com>
2024-08-04 11:59:48 -0600

    updates

---
+++ MODULE3/discussion-3-6.py
@@ -0,0 +1,17 @@
+# Array that holds grades for a course
+grades = [90, 100, 80, 95, 78]
+
+def add_grade(grade):
+    grades.append(grade)
+
+def get_average():
+    total = 0
+    for grade in grades:
+        if isinstance(grade, int):
+            total += grade
+    grade_average = total / len(grades)
+    return grade_average
+
+print("Grade Average is:", get_average())
+add_grade(100)
+print("New Grade Average is:", get_average())
