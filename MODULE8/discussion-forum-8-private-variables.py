519311f16eaf3d77496ecedee13872e8c672a85b
sandhu <e442532@us.lmco.com>
2024-09-05 11:02:36 -0600

    UPDATES

--- MODULE8/discussion-forum-8-private-variables.py
+++ MODULE8/discussion-forum-8-private-variables.py
@@ -1,8 +1,8 @@
 class CoffeeShopItem:
     def __init__(self, name, price):
         self.__name = None
-        self.__name = name          # Private attribute
-        self.__price = price        # Private attribute
+        self.__name = name  # Private attribute
+        self.__price = price  # Private attribute
 
     def display_info(self):
         print(f"Item: {self.__name}, Price: ${self.__price:.2f}")
@@ -21,6 +21,7 @@ class CoffeeShopItem:
             self.__price = price
         else:
             print("Invalid price")
+
 
 # Creating instances of different items in the coffee shop
 frappuccino = CoffeeShopItem("Frappuccino", 4.50)
@@ -44,8 +45,8 @@ frappuccino.set_price(4.75)
 print(frappuccino.get_price())  # Output: 4.75
 
 # Displaying information for all items
-coffee.display_info()                 # Output: Item: Coffee, Price: $2.00
+coffee.display_info()  # Output: Item: Coffee, Price: $2.00
 creme_brulee_iced_coffee.display_info()  # Output: Item: Creme Brulee Iced Coffee, Price: $5.75
-banana_cake.display_info()            # Output: Item: Banana Cake, Price: $3.25
-muffin.display_info()                 # Output: Item: Muffin, Price: $2.75
-yogurt.display_info()                 # Output: Item: Yogurt, Price: $1.50
+banana_cake.display_info()  # Output: Item: Banana Cake, Price: $3.25
+muffin.display_info()  # Output: Item: Muffin, Price: $2.75
+yogurt.display_info()  # Output: Item: Yogurt, Price: $1.50
